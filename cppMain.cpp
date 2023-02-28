55639ec35afaa16128492b09fb533c42d0098e4f
ryan-montoya <rmonto6@uw.edu>
2023-02-28 00:21:01 -0800

    first commit

---
+++ cppMain.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char *argv[]) { 
+
+    cout << "Hello!"; 
+
+    return 0;
+
+
+}
---
+++ rustmain.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+
+    function_2();
+}
+
+
+
+
+fn function_2() {
+    println!("Another function.");
+} 
+
+
+
+fn function_3() {  
+    println!("beans");
+}
