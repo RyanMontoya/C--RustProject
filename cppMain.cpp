cbddb6510786b88ccacd6c09484dd7e257c9dd80
ryan-montoya <rmonto6@uw.edu>
2023-02-28 22:58:46 -0800

    Added a small class.

--- cppMain.cpp
+++ cppMain.cpp
@@ -3,12 +3,38 @@
 #include <iomanip>
 #include <cstdlib>
 using namespace std;
+//class cppMain {  
+
+
+ // void printExtra() { 
+    //    std::cout << "extra";
+    //}
+
+class extra { 
+    
+public:
+ void printExtra() { 
+     std::cout << "Hello EXTRA!";
+}
+};
+
+
 
 int main() { 
-
+    //printExtra(); 
+    extra example;
+    example.printExtra();
     std::cout << "Hello!" << std::endl; 
 
     return 0;
 
 
-}
+  } 
+
+
+
+
+//};
+
+
+
